f9325c2735afd6e100e688392fed9fac3d78a860
d1ll0n <dillonkellar@protonmail.com>
2019-04-21 19:12:20 -0500

    added mainnet contract builder

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ const BNS = require('./bns')
 const abi = require('./abi.json')
 
 const ropstenAddress = '0xbc192f3a81bca5b057928e16a6ade1c7abc67077'
+const mainnetAddress = '0x15dA427d0c2119d2B96CECB0CE1884f0077B24eA'
 
 module.exports = {
-  ropsten: (web3, account) => new BNS(web3, account, abi, ropstenAddress)
+  ropsten: (web3, account) => new BNS(web3, account, abi, ropstenAddress),
+  mainnet: (web3, account) => new BNS(web3, account, abi, mainnetAddress)
 }
